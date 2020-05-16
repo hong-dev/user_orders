8a3ffda09a5de0546811190e67d2ed30c3126b65
hong-dev <jjuggumih@gmail.com>
2020-05-17 03:14:59 +0900

    create order detail view 1. 특정 회원의 주문 정보 get하는 view 작성     * 특정 회원의 id(pk)를 querystring으로 url에 담아서 요청     * 결제일시(payment_date) 기준 최신순으로 정렬하여 return 2. OrderDetailView의 unit test 작성

--- order/tests.py
+++ order/tests.py
@@ -16,6 +16,7 @@ class OrderTest(TestCase):
         )
 
         User.objects.create(
+            id           = 1,
             name         = "홍",
             nickname     = "Dev",
             password     = bcrypt.hashpw("1234".encode('utf-8'), bcrypt.gensalt()).decode('utf-8'),
@@ -92,3 +93,64 @@ class OrderTest(TestCase):
             }
         )
         self.assertEqual(response.status_code, 400)
+
+class OrderDetailTest(TestCase):
+    def setUp(self):
+        Gender.objects.create(
+            id     = 1,
+            gender = "Woman"
+        )
+
+        User.objects.create(
+            id           = 1,
+            name         = "홍",
+            nickname     = "Dev",
+            password     = "1234",
+            phone_number = "01012345678",
+            email        = "hong@gamil.com",
+            gender       = Gender.objects.get(id = 1)
+        )
+
+        Order.objects.create(
+            id           = 1,
+            user         = User.objects.get(id = 1),
+            order_number = "8UVNSN0Z9IG4",
+            product      = "비누",
+            payment_date = "2019-03-01T00:24:00"
+        )
+
+        Order.objects.create(
+            id           = 2,
+            user         = User.objects.get(id = 1),
+            order_number = "MDT30FXBKY5J",
+            product      = "책갈피",
+            payment_date = "2020-05-01"
+        )
+
+    def tearDown(self):
+        Gender.objects.all().delete()
+        User.objects.all().delete()
+        Order.objects.all().delete()
+
+    def test_order_detail_get_success(self):
+        response = Client().get('/order/detail?user=1')
+
+        self.assertEqual(response.json(),
+            {
+                "orders" : [
+                    {
+                        "id"           : 2,
+                        "order_number" : "MDT30FXBKY5J",
+                        "product"      : "책갈피",
+                        "payment_date" : "2020-05-01T00:00:00"
+                    },
+                    {
+                        "id"           : 1,
+                        "order_number" : "8UVNSN0Z9IG4",
+                        "product"      : "비누",
+                        "payment_date" : "2019-03-01T00:24:00"
+                    }
+                ]
+            }
+        )
+        self.assertEqual(response.status_code, 200)
--- order/urls.py
+++ order/urls.py
@@ -1,7 +1,8 @@
-from .views import OrderView
+from .views import OrderView, OrderDetailView
 
 from django.urls import path
 
 urlpatterns = [
-    path('', OrderView.as_view())
+    path('', OrderView.as_view()),
+    path('/detail', OrderDetailView.as_view())
 ]
