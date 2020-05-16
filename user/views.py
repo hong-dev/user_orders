ef605ccf291ba43cda9eb125731f5204072f71f0
hong-dev <jjuggumih@gmail.com>
2020-05-16 22:04:43 +0900

    create sign-up view 1. 회원가입 view 작성     * 필수: 이름, 닉네임, 비밀번호, 전화번호, 이메일     * 선택: 성별 2. 회원가입 view의 unit test 코드 작성     * 회원가입 성공 case (성별 선택했을 때)     * 회원가입 성공 case (성별 선택하지 않았을 때)     * 필수 입력 요소들이 입력되지 않았을 때 각각 실패 case

--- user/views.py
+++ user/views.py
@@ -1,3 +1,39 @@
-from django.shortcuts import render
+import json
+import bcrypt
+import jwt
 
-# Create your views here.
+from project.settings import SECRET_KEY
+from .models          import Gender, User
+
+from django.views import View
+from django.http  import HttpResponse, JsonResponse
+
+class SignUpView(View):
+    def post(self, request):
+        user_data = json.loads(request.body)
+
+        gender = user_data.get('gender')
+
+        try:
+            if User.objects.filter(email = user_data['email']).exists():
+                return JsonResponse({"error" : "EMAIL_ALREADY_EXISTS"}, status = 400)
+
+            User.objects.create(
+                name         = user_data['name'],
+                nickname     = user_data['nickname'],
+                password     = bcrypt.hashpw(
+                    user_data['password'].encode('utf-8'),
+                    bcrypt.gensalt()
+                ).decode('utf-8'),
+                phone_number = user_data['phone_number'],
+                email        = user_data['email'],
+                gender       = Gender.objects.get(gender = gender) if gender else None
+            )
+
+            return HttpResponse(status = 200)
+
+        except KeyError:
+            return JsonResponse({"error" : "INVALID_KEYS"}, status = 400)
+
+        except Gender.DoesNotExist:
+            return JsonResponse({"error" : "GENDER_DOES_NOT_EXIST"}, status = 400)
